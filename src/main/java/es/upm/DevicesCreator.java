3631332b66cb78fba146b13b3b4d15145fd30694
gringosim <ezequielsimeoni@gmail.com>
2018-08-03 17:27:01 +0200

    modificado para inclusión automática de id de DB en Document al crearlo

---
+++ src/main/java/es/upm/DevicesCreator.java
@@ -0,0 +1,4 @@
+package es.upm;
+
+public class DevicesCreator {
+}
--- src/main/java/es/upm/interfaces/LivingLabDevice.java
+++
@@ -1,6 +0,0 @@
-package es.upm.interfaces;
-
-public interface LivingLabDevice {
-    public Object getDeviceStatus(Object device);
-    public Object setDeviceStatus(Object device,Object newStatus);
-}
